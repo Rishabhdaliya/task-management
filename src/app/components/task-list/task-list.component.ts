1bf6ee51bcd2c41d812e717bd2224164635f381a
Rishabhdaliya <14rishabhdaliya@gmail.com>
2023-07-28 22:53:03 +0530

    feat: Initial commit

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -7,18 +7,39 @@ import { Task } from '../../task.modal';
   styleUrls: ['./task-list.component.css'],
 })
 export class TaskListComponent {
-  tasks: Task[] = [
-    { id: 1, title: 'Buy groceries done ', isCompleted: false },
+  allTasks: Task[] = [
+    { id: 1, title: 'Buy groceries done', isCompleted: false },
     { id: 2, title: 'Walk the dog', isCompleted: true },
     { id: 3, title: 'Read a book', isCompleted: false },
   ];
 
+  // Create a copy of allTasks that will be used for filtering
+  tasks: Task[] = this.allTasks.slice();
+
+  filterType: 'all' | 'completed' | 'incomplete' = 'all';
+
   handleAddTask(newTaskTitle: string) {
     const newTask = {
-      id: this.tasks.length + 1,
+      id: this.allTasks.length + 1,
       title: newTaskTitle,
       isCompleted: false,
     };
-    this.tasks.push(newTask);
+    this.allTasks.push(newTask);
+    this.filterTasks(this.filterType); // Reapply the current filter after adding a new task
+  }
+
+  filterTasks(type: 'all' | 'completed' | 'incomplete') {
+    this.filterType = type;
+    switch (type) {
+      case 'completed':
+        this.tasks = this.allTasks.filter((task) => task.isCompleted);
+        break;
+      case 'incomplete':
+        this.tasks = this.allTasks.filter((task) => !task.isCompleted);
+        break;
+      default:
+        this.tasks = this.allTasks.slice();
+        break;
+    }
   }
 }
